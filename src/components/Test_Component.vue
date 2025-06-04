c17d6d66a5014b006816dd9ba05b1b2c4b42097d
De Gonzague <comoenoelmariefrancois@gmail.com>
2025-06-04 12:28:27 +0000

    home section rectification and resolved the bog

--- src/components/Test_Component.vue
+++ src/components/Test_Component.vue
@@ -1,29 +1,3 @@
 <template>
-  <div
-    class="top-bubble-wrap is-taller"
-    :style="topBubbleWrapStyle"
-  >
-    <div
-      class="top-bubble"
-      :style="topBubbleStyle"
-    ></div>
-  </div>
+  <div></div>
 </template>
-
-<script setup lang="ts">
-import { reactive } from 'vue'
-
-// Définition réactive des styles transform pour chaque div
-const topBubbleWrapStyle = reactive({
-  willChange: 'transform',
-  transform: 'translate3d(0px, -1.7996vw, 0px) scale3d(1, 1, 1) rotateX(0deg) rotateY(0deg) rotateZ(0deg) skew(0deg)',
-  transformStyle: 'preserve-3d',
-})
-
-const topBubbleStyle = reactive({
-  willChange: 'transform',
-  transform: 'translate3d(0px, 0px, 0px) scale3d(1, 1, 1) rotateX(0deg) rotateY(42.9755deg) rotateZ(0deg) skew(0deg)',
-  transformStyle: 'preserve-3d',
-})
-</script>
-
